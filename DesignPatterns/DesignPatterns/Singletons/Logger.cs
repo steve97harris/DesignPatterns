c9d2a624312d6cd60ce8ace4e943dc3a9ea1181b
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-02-07 21:48:39 +0000

    kjbkhjb

---
+++ DesignPatterns/DesignPatterns/Singletons/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.singletons
+{
+    public class Logger
+    {
+        private static Logger logger;
+
+        private Logger()
+        {
+
+        }
+
+        public static Logger GetInstance()
+        {
+            if (logger == null)
+            {
+                logger = new Logger();
+            }
+            return logger;
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/Singletons/LoggerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.singletons
+{
+    class Logger        // Logger, used to create a customized error log file or log specific events.
+    {
+        static void Main()
+        {
+            // Logger object1 = new Logger();
+            // Logger object2 = new Logger();
+            Logger object1 = Logger.GetInstance();
+            Logger object2 = Logger.GetInstance();
+            Console.WriteLine(object1.GetHashCode());
+            Console.WriteLine(object2.GetHashCode());
+        }
+    }
+}
