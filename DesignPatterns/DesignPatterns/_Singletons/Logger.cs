f72c258fcd911b46e38cff34a6d0b6c10a6a254b
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-02-09 14:07:54 +0000

    asd

--- DesignPatterns/DesignPatterns/_Singletons/Logger.cs
+++ DesignPatterns/DesignPatterns/_Singletons/Logger.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace DesignPatterns.singletons
+namespace DesignPatterns._singletons
 {
     public class Logger
     {
--- DesignPatterns/DesignPatterns/_Singletons/TestLogger.cs
+++ DesignPatterns/DesignPatterns/_Singletons/TestLogger.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace DesignPatterns.singletons
+namespace DesignPatterns._singletons
 {
     class TestLogger        // Logger, used to create a customized error log file or log specific events.
     {
